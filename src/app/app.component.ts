2efd40fa0a685ad6eb4cf3f4b85980ba61e33951
pranayXoX <pranay.dmnsns@gmail.com>
2023-03-28 21:56:57 +0530

    output block complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,26 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'post_app';
+  postTitle: String = '';
+  postDesc: String = '';
+  imgUrl: String = '';
+  webUrl: String = '';
+  getTitle(value: String) {
+    this.postTitle = value;
+    console.log(this.postTitle);
+  }
+  getDesc(value: String) {
+    this.postDesc = value;
+    console.log(this.postDesc);
+  }
+  getImg(value: String) {
+    this.imgUrl = value;
+  }
+  getLink(value: String) {
+    this.webUrl = value;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { OutputBlockComponent } from './output-block/output-block.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, OutputBlockComponent],
+  imports: [BrowserModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
